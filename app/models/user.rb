66e70059d7cd8d9d9cf4fda57b150a92f9c9042a
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-09 01:22:26 +0300

    event views and navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 35}
   validates :email, presence: true, length: {maximum: 255}
   validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+  # validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   before_validation :set_name, on: :create
 
   private
   def set_name
-    self.name = 'Somebody #{rand(111)}' if self.name.blank?
+    self.name = "Somebody #{rand(111)}" if self.name.blank?
   end
 end
