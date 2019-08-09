49cbfa760aec8e36577db0ddf40371296b6fa0a4
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-10 00:45:09 +0300

    delete devise views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create([
+#                 {name: 'Kat', email: 'kkk@k.k', password: '123456'},
+#                 {name: 'Fat', email: 'fff@f.f', password: '123456'},
+#                 {name: 'Rat', email: 'rrr@r.r', password: '123456'},
+#                 {name: 'Wat', email: 'www@w.w', password: '123456'}
+#             ]
+# )
+
