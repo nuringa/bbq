58fcc7bf2a6b2cb55b45cd2fe8b5d1a145fd3ca9
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-13 02:18:16 +0300

    comments controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 35}
   validates :email, presence: true, length: {maximum: 255}
   validates :email, uniqueness: true
-  # validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   before_validation :set_name, on: :create
 
