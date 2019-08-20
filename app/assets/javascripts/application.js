98a3b8c01b772d6944bb8d163c4d15db0ffe2c4d
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-20 12:14:26 +0300

    jquery fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require twitter/bootstrap
 //= require jquery
-//= require jquery_ujs
 //= require rails-ujs
+// = require twitter/bootstrap
 //= require activestorage
 //= require_tree .
