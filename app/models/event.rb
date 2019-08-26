8b70565de34f652aea33f4403ac348b1be648b89
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-26 23:32:49 +0300

    event delete fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :comments
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
