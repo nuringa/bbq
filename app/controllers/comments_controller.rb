1071b129f775b1873001b4b3d17eb5d3b3aeeb4b
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-28 21:16:25 +0300

    not notify author of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,8 +38,9 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
-   all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-   author_email = current_user.email
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails.delete(current_user.email) if current_user
+
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_now
     end
