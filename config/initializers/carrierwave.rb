17aa2e07ac9a013fb38d9b1c39d2ad3dffbf4735
admin.karima@gmail.com <admin.karima@gmail.com>
2019-10-10 22:58:33 +0300

    credentials for deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,10 +6,10 @@ if Rails.env.production?
     config.fog_credentials = {
         provider:              'AWS',
         region:              'us-east-2',
-        aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-        aws_secret_access_key: ENV['S3_SECRET_KEY']
+        aws_access_key_id:     Bbq::Application.credentials.s3_access_key,
+        aws_secret_access_key: Bbq::Application.credentials.s3_secret_key
     }
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_directory  = Bbq::Application.credentials.s3_bucket_name
   end
 end
