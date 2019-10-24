83752d23395e6e1ea561ba8da01fca9ec886687e
admin.karima@gmail.com <admin.karima@gmail.com>
2019-10-24 23:46:10 +0300

    pundit event policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_user_can_edit?
 
@@ -13,5 +17,12 @@ class ApplicationController < ActionController::Base
     model.user == current_user ||
         (model.try(:event).present? && model.event.user == current_user)
     )
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = t('pundit.not_authorized')
+    redirect_to(request.referrer || root_path)
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,17 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_event, only: [:show]
-  before_action :set_current_user_event, only: [:edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   before_action :password_guard!, only: [:show]
+  after_action :verify_authorized, only: [:edit, :update, :destroy, :show]
 
   def index
     @events = Event.all
   end
 
   def show
+    authorize @event
+
     @new_comment = @event.comments.build(params[:comment])
     @new_subscription = @event.subscriptions.build(params[:subscription])
     @new_photo = @event.photos.build(params[:photo])
@@ -20,6 +22,7 @@ class EventsController < ApplicationController
   end
 
   def edit
+    authorize @event
   end
 
   def create
@@ -33,6 +36,8 @@ class EventsController < ApplicationController
   end
 
   def update
+    authorize @event
+
     if @event.update(event_params)
       redirect_to @event, notice: I18n.t('controllers.events.updated')
     else
@@ -41,15 +46,13 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    authorize @event
+
     @event.destroy
     redirect_to events_url, notice: I18n.t('controllers.events.destroyed')
   end
 
   private
-  def set_current_user_event
-    @event = current_user.events.find(params[:id])
-  end
-
   def set_event
     @event = Event.find(params[:id])
   end
