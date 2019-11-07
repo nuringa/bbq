a5f6c5765d633b6da881e6099a8a713f22f2db5c
admin.karima@gmail.com <admin.karima@gmail.com>
2019-11-07 15:19:13 +0300

    clean comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,20 +22,16 @@ class User < ApplicationRecord
   end
 
   def self.find_for_facebook_oauth(access_token)
-    # Достаём email из токена
     email = access_token.info.email
     user = where(email: email).first
 
-    # Возвращаем, если нашёлся
     return user if user.present?
 
-    # Если не нашёлся, достаём провайдера, айдишник и урл
     provider = access_token.provider
     id = access_token.extra.raw_info.id
     url = "https://facebook.com/#{id}"
 
     where(url: url, provider: provider).first_or_create! do |user|
-      # Если создаём новую запись, прописываем email и пароль
       user.email = email
       user.password = Devise.friendly_token.first(16)
     end
